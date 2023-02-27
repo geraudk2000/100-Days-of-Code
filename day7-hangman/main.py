67e5c4164d8b206e241c6e58fab0e890e8d200ff
Geraud <geraudkameni@gmail.com>
2023-02-27 19:36:42 +0100

    python code for hangman

--- day7-hangman/main.py
+++ day7-hangman/main.py
@@ -6,20 +6,20 @@ from hangman_art import stages, logo
 
 print(logo)
 
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
 lives = 6 
 stopGame = False
 display = []
 letter_guess_before = ""
+#Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 word = random.choice(word_list)
+
 print(f"the chosen word is {word}") 
 
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 for x in range(len(word)):
     display.append("_")
 
 while not stopGame:
+  #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
   guess_letter = input("Guess a letter: ").lower()
   
   if guess_letter in letter_guess_before: 
@@ -27,12 +27,11 @@ while not stopGame:
   else:
     letter_guess_before += guess_letter
     print(f"You gess {guess_letter}")
+    #Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
     if guess_letter not in word:
       lives -= 1 
 
-
-  #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in     the chosen_word.
-  print(lives)
+  #print(lives)
   print(stages[lives])
   
   for position in range(len(word)): 
@@ -45,8 +44,6 @@ while not stopGame:
     stopGame = False
   else:  
     stopGame = True
-
-
 
 if "_" in display:
   print("You loose, try again.")
