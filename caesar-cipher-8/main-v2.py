144ae30a1cbbcd30c47aa7578de4bc3a9c26fbdc
Geraud <geraudkameni@gmail.com>
2023-02-28 15:11:05 +0100

    caesar code

--- caesar-cipher-8/main-v2.py
+++ caesar-cipher-8/main-v2.py
@@ -4,22 +4,30 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 # to solve end of list of alphabet, duplicate the alphabet
 
 print(logo)
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-print(logo)
+continue_game = True
 
 def caesar(direction, text, shift):
     caesar_text = ""
     if direction == "decode":
         shift *= -1 
-    for letter in text:
-        position = alphabet.index(letter)
-        caesar_text += alphabet[position+shift]
+    for char in text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            caesar_text += alphabet[position+shift]
+        else:
+            caesar_text += char
     print(f"your {direction} message is --> " + caesar_text)
- 
+    
     return caesar_text
 
-caesar(direction, text, shift)
+while continue_game:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(direction, text, shift)
+    
+    again = input("Do you want to continue? pres yes or any keys to exit ")
+
+    if again != 'yes':
+        print("Goodbye!!")
+        continue_game = False
